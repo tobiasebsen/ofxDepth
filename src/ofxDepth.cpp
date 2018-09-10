1ebc43bdc4bec59c3d32e1f346f8fc0a2cab0eb9
tobiasebsen <tobiasebsen@gmail.com>
2018-09-10 07:28:38 +0200

    Major overhaul

--- src/ofxDepth.cpp
+++ src/ofxDepth.cpp
@@ -3,82 +3,328 @@
 #define STRINGIFY(A) #A
 
 string depthProgram = STRINGIFY(
-__kernel void map(__global unsigned short* depth, unsigned short imin, unsigned short imax, unsigned short omin, unsigned short omax) {
+
+__kernel void denoise(__global unsigned short* input, __global unsigned short* output, float threshold, int neighbours) {
+    int2 coords = (int2)(get_global_id(0), get_global_id(1));
+    int width = get_global_size(0);
+    int height = get_global_size(1);
+    int i = coords.y * width + coords.x;
+    
+    int xmin = coords.x >= 1 ? coords.x-1 : 0;
+    int xmax = coords.x < width-1 ? coords.x+1 : width-1;
+    int ymin = coords.y >= 1 ? coords.y-1 : 0;
+    int ymax = coords.y < height-1 ? coords.y+1 : height-1;
+    
+    int sum = 0;
+
+    for (int y=ymin; y<=ymax; y++) {
+        for (int x=xmin; x<=xmax; x++) {
+            int j = y * width + x;
+            if (i != j && abs(input[i] - input[j]) < threshold) {
+                sum++;
+            }
+        }
+    }
+    if (sum < neighbours)
+        output[i] = 0;
+    else
+        output[i] = input[i];
+};
+
+__kernel void map(__global unsigned short* depthIn, unsigned short imin, unsigned short imax, unsigned short omin, unsigned short omax, __global unsigned short* depthOut) {
 	int2 coords = (int2)(get_global_id(0), get_global_id(1));
 	int i = coords.y * get_global_size(0) + coords.x;
-	depth[i] = (depth[i] - imin) * ((omax - omin) / (imax - imin)) + omin;
+    if (depthIn[i] == 0)
+        depthOut[i] = 0;
+    else
+        depthOut[i] = (( (int)depthIn[i] - imin) * ( (int)omax - omin)) / ( (int)imax - imin) + omin;
 }
 
-__kernel void toPoints(__global unsigned short* depth, float2 fov, __global float4* points) {
+__kernel void pointsFromFov(__global unsigned short* depth, float2 fov, __global float4* points) {
     int2 coords = (int2)(get_global_id(0), get_global_id(1));
     int2 dims = (int2)(get_global_size(0), get_global_size(1));
     float2 angle = fov * (((float2)coords / dims) - float2(0.5f));
     int i = coords.y * dims.x + coords.x;
     points[i].x = tan(radians(angle.x)) * depth[i];
     points[i].y = tan(radians(angle.y)) * depth[i];
-    points[i].z = depth[i];
+    points[i].z = -depth[i];
     points[i].w = 0;
 }
+                                
+__kernel void pointsFromTable(__global unsigned short* depth, __global float2* table, __global float4* points) {
+    int2 coords = (int2)(get_global_id(0), get_global_id(1));
+    int i = coords.y * get_global_size(0) + coords.x;
+    unsigned short d = depth[i];
+    points[i].x = table[i].x * d;
+    points[i].y = table[i].y * d;
+    points[i].z = -d;
+    points[i].w = 0.f;
+};
+
+__kernel void transform(__global float4 *input, __global float4 *output, __global float4 *mat) {
+    int2 coords = (int2)(get_global_id(0), get_global_id(1));
+    int width = get_global_size(0);
+    int i = coords.y * width + coords.x;
+    float4 v = input[i];
+    
+    output[i].x = mat[0].x * v.x + mat[1].x * v.y + mat[2].x * v.z + mat[3].x;
+    output[i].y = mat[0].y * v.x + mat[1].y * v.y + mat[2].y * v.z + mat[3].y;
+    output[i].z = mat[0].z * v.x + mat[1].z * v.y + mat[2].z * v.z + mat[3].z;
+}
+                                
 );
 
-void ofxDepth::setup(int width, int height, int deviceNumber) {
-	this->width = width;
-	this->height = height;
-	opencl.setupFromOpenGL(deviceNumber);
-	opencl.loadProgramFromSource(depthProgram);
-	opencl.loadKernel("map");
-    opencl.loadKernel("toPoints");
-	if (width != 0 && height != 0) {
-		texObj.allocate(width * height * sizeof(unsigned short), GL_STREAM_DRAW);
-		depth.initFromGLObject(texObj.getId());
-        pntObj.allocate(width * height * sizeof(ofVec4f), GL_STREAM_DRAW);
-        points.initFromGLObject(pntObj.getId());
-	}
+void ofxDepthImage::allocate(int width, int height) {
+    this->width = width;
+    this->height = height;
+    glBuf.allocate(width * height * sizeof(unsigned short), GL_STREAM_DRAW);
+    clBuf.initFromGLObject(glBuf.getId());
 }
 
-void ofxDepth::write(ofShortPixels & p) {
-	if (width == 0 || height == 0) {
-		texObj.allocate(p.getWidth() * p.getHeight() * sizeof(unsigned short), GL_STREAM_DRAW);
-		depth.initFromGLObject(texObj.getId());
-		//depth.initBuffer(p.getWidth() * p.getHeight() * sizeof(unsigned short));
-	}
-	width = p.getWidth();
-	height = p.getHeight();
-	depth.write(p.getData(), 0, width * height * p.getBytesPerPixel());
+bool ofxDepthImage::isAllocated() const {
+    return glBuf.isAllocated();
 }
 
-void ofxDepth::read(ofShortPixels & p) {
-	depth.read(p.getData(), 0, p.getWidth() * p.getHeight() * p.getBytesPerPixel());
+void ofxDepthImage::load(string filepath) {
+    ofxDepth.setup();
+    ofShortPixels pixels;
+    ofLoadImage(pixels, filepath);
+    ofShortPixels spixels = pixels.getChannel(0);
+    write(spixels);
 }
 
-void ofxDepth::updateTexture(ofTexture & tex) {
-	texObj.bind(GL_PIXEL_UNPACK_BUFFER);
-	tex.loadData((unsigned short*)NULL, width, height, GL_LUMINANCE);
-	texObj.unbind(GL_PIXEL_UNPACK_BUFFER);
+void ofxDepthImage::write(ofShortPixels & pixels) {
+    if (!isAllocated())
+        allocate(pixels.getWidth(), pixels.getHeight());
+    clBuf.write(pixels.getData(), 0, pixels.getWidth() * pixels.getHeight() * pixels.getBytesPerPixel());
 }
 
-void ofxDepth::map(uint16_t inputMin, uint16_t inputMax, uint16_t outputMin, uint16_t outputMax) {
-	OpenCLKernelPtr kernel = opencl.kernel("map");
-	kernel->setArg(0, depth);
-	kernel->setArg(1, &inputMin, sizeof(uint16_t));
-	kernel->setArg(2, &inputMax, sizeof(uint16_t));
-	kernel->setArg(3, &outputMin, sizeof(uint16_t));
-	kernel->setArg(4, &outputMax, sizeof(uint16_t));
-	kernel->run2D(width, height);
+void ofxDepthImage::read(ofShortPixels & pixels) {
+    clBuf.read(pixels.getData(), 0, pixels.getWidth() * pixels.getHeight() * pixels.getBytesPerPixel());
 }
 
-void ofxDepth::toPoints(float fovH, float fovV, OpenCLBuffer & points) {
-    OpenCLKernelPtr kernel = opencl.kernel("toPoints");
-    kernel->setArg(0, depth);
-    kernel->setArg(1, ofVec2f(fovH, fovV));
-    kernel->setArg(2, points);
+void ofxDepthImage::update(ofTexture & tex) {
+    glBuf.bind(GL_PIXEL_UNPACK_BUFFER);
+    tex.loadData((unsigned short*)NULL, width, height, GL_LUMINANCE);
+    glBuf.unbind(GL_PIXEL_UNPACK_BUFFER);
+}
+
+void ofxDepthImage::update() {
+    update(tex);    
+}
+
+void ofxDepthImage::draw(float x, float y) {
+    tex.draw(x, y);
+}
+
+void ofxDepthImage::draw(float x, float y, float w, float h) {
+    tex.draw(x, y, w, h);
+}
+
+void ofxDepthImage::denoise(float threshold, int neighbours, ofxDepthImage &outputImage) {
+    OpenCLKernelPtr kernel = ofxDepth.getKernel("denoise");
+    kernel->setArg(0, getCLBuffer());
+    kernel->setArg(1, outputImage.getCLBuffer());
+    kernel->setArg(2, threshold);
+    kernel->setArg(3, neighbours);
     kernel->run2D(width, height);
 }
 
-void ofxDepth::toPoints(float fovH, float fovV) {
-    if (!pntObj.isAllocated()) {
-        pntObj.allocate(width * height * sizeof(ofVec4f), GL_STREAM_DRAW);
-        points.initFromGLObject(pntObj.getId());
+void ofxDepthImage::denoise(float threshold, int neighbours) {
+    denoise(threshold, neighbours, *this);
+}
+
+void ofxDepthImage::map(uint16_t inputMin, uint16_t inputMax, uint16_t outputMin, uint16_t outputMax, ofxDepthImage & outpuImage) {
+    
+    if (!outpuImage.isAllocated())
+        outpuImage.allocate(getWidth(), getHeight());
+    
+    OpenCLKernelPtr kernel = ofxDepth.getKernel("map");
+    kernel->setArg(0, getCLBuffer());
+    kernel->setArg(1, &inputMin, sizeof(uint16_t));
+    kernel->setArg(2, &inputMax, sizeof(uint16_t));
+    kernel->setArg(3, &outputMin, sizeof(uint16_t));
+    kernel->setArg(4, &outputMax, sizeof(uint16_t));
+    kernel->setArg(5, outpuImage.getCLBuffer());
+    kernel->run2D(getWidth(), getHeight());
+}
+
+void ofxDepthImage::map(uint16_t inputMin, uint16_t inputMax, uint16_t outputMin, uint16_t outputMax) {
+    map(inputMin, inputMax,  outputMin, outputMax, *this);
+}
+
+void ofxDepthImage::toPoints(float fovH, float fovV, ofxDepthPoints & points) {
+    
+    if (!points.isAllocated())
+        points.allocate(getNumPixels());
+
+    OpenCLKernelPtr kernel = ofxDepth.getKernel("pointsFromFov");
+    kernel->setArg(0, getCLBuffer());
+    kernel->setArg(1, ofVec2f(fovH, fovV));
+    kernel->setArg(2, points.getCLBuffer());
+    kernel->run2D(getWidth(), getHeight());
+    
+    points.setCount(getNumPixels());
+}
+
+//////////////////////////////////////////////////
+
+void ofxDepthData::allocate(int size) {
+    data.resize(size);
+}
+
+int ofxDepthData::countZeros() {
+    int zeroCount = 0;
+    for (ofVec4f & p : data) {
+        if (p.x == 0 && p.y == 0 && p.z == 0)
+            zeroCount++;
     }
-    toPoints(fovH, fovV, points);
+    return zeroCount;
+}
+
+int ofxDepthData::removeZeros(bool resize) {
+    removeZeros(resize, *this);
+}
+
+int ofxDepthData::removeZeros(bool resize, ofxDepthData &outputData) {
+    outputData.allocate(getSize());
+    ofVec4f * pData = outputData.getData().data();
+    int nz = 0;
+    for (ofVec4f & p : data) {
+        if (p.x != 0 || p.y != 0 || p.z != 0) {
+            data[nz] = p;
+            nz++;
+        }
+    }
+    outputData.setCount(nz);
+    if (resize)
+        outputData.allocate(nz);
+    return nz;
+}
+
+//////////////////////////////////////////////////
+
+void ofxDepthPoints::allocate(int size) {
+    this->size = 0;
+    glBuf.allocate(size * sizeof(ofVec4f), GL_STREAM_DRAW);
+    vbo.setVertexBuffer(glBuf, 3, sizeof(ofVec4f));
+    clBuf.initFromGLObject(glBuf.getId());
+}
+
+bool ofxDepthPoints::isAllocated() {
+    return glBuf.isAllocated();
+}
+
+void ofxDepthPoints::load(string filepath) {
+    ofFile file(filepath);
+    string line;
+    int width = 0;
+    int height = 0;
+    int points = 0;
+    while(std::getline(file, line) && line.find("DATA") != 0) {
+        vector<string> parts = ofSplitString(line, " ");
+        if (parts[0] == "WIDTH")
+            width = ofToInt(parts[1]);
+        if (parts[0] == "HEIGHT")
+            height = ofToInt(parts[1]);
+        if (parts[0] == "POINTS")
+            points = ofToInt(parts[1]);
+        cout << line << endl;
+    }
+    if (line.find("DATA") == 0) {
+        size_t pos = file.tellg();
+        uint64_t s = file.getSize();
+        uint64_t n = (s - pos) / sizeof(float);
+        vector<ofVec3f> dat;
+        dat.resize(points);
+    }
+}
+
+void ofxDepthPoints::read(ofxDepthData & data) {
+    read(data.getData());
+    data.setCount(getCount());
+}
+
+void ofxDepthPoints::read(vector<ofVec4f> & points) {
+    points.resize(getCount());
+    clBuf.read(points.data(), 0, points.size() * sizeof(ofVec4f));
+}
+
+void ofxDepthPoints::write(ofxDepthData & data) {
+    write(data.getData(), data.getCount());
+}
+
+void ofxDepthPoints::write(vector<ofVec4f> & points) {
+    write(points, points.size());
+}
+
+void ofxDepthPoints::write(vector<ofVec4f> & points, int count) {
+    clBuf.write(points.data(), 0, count * sizeof(ofVec4f));
+    this->count = count;
+}
+
+void ofxDepthPoints::draw() {
+    vbo.draw(GL_POINTS, 0, count);
+}
+
+void ofxDepthPoints::transform(const ofMatrix4x4 &mat, ofxDepthPoints &outputPoints) {
+    
+    if (matrix.size() == 0) {
+        matrix.initBuffer(4);
+    }
+    
+    for (int i=0; i<4; i++) {
+        matrix[i] = mat.getRowAsVec4f(i);
+    }
+    matrix.writeToDevice();
+    
+    OpenCLKernelPtr kernel = ofxDepth.getKernel("transform");
+    kernel->setArg(0, getCLBuffer());
+    kernel->setArg(1, outputPoints.getCLBuffer());
+    kernel->setArg(2, matrix);
+    kernel->run1D(count);
+}
+
+void ofxDepthPoints::transform(const ofMatrix4x4 &mat) {
+    transform(mat, *this);
+}
+
+//////////////////////////////////////////////////
+
+void ofxDepthCore::setup(int deviceNumber) {
+
+    if (isSetup())
+        return;
+
+	opencl.setupFromOpenGL(deviceNumber);
+	opencl.loadProgramFromSource(depthProgram);
+	opencl.loadKernel("map");
+    opencl.loadKernel("pointsFromFov");
+    opencl.loadKernel("pointsFromTable");
+    opencl.loadKernel("transform");
+    opencl.loadKernel("denoise");
+}
+
+void ofxDepthCore::setup(string vendorName, string deviceName) {
+    int n = opencl.getDeviceInfos();
+    for (int i=0; i<n; i++) {
+        if ((string((char*)opencl.deviceInfo[i].vendorName) == vendorName && deviceName == "") ||
+            string((char*)opencl.deviceInfo[i].deviceName) == deviceName) {
+            setup(i);
+            return;
+        }
+    }
+}
+
+bool ofxDepthCore::isSetup() {
+    return opencl.getDevice() != nullptr;
+}
+
+OpenCL & ofxDepthCore::getCL() {
+    setup();
+    return opencl;
+}
+
+OpenCLKernelPtr ofxDepthCore::getKernel(string name) {
+    return getCL().kernel(name);
 }
--- src/ofxDepth.h
+++ src/ofxDepth.h
@@ -4,27 +4,178 @@
 
 using namespace msa;
 
-class ofxDepth {
+class ofxDepthPoints;
+
+//////////////////////////////////////////////////
+// DEPTH TABLE
+//
+// Depth (z) to world position table
+
+class ofxDepthTable {
 public:
-	void setup(int width = 0, int height = 0, int deviceNumber = -1);
-
-	void write(ofShortPixels & pixels);
-	void read(ofShortPixels & pixels);
+    void allocate(int width, int height);
     
-    void drawPoints();
+    void write(ofFloatPixels & entries);
 
-	void updateTexture(ofTexture & tex);
+    OpenCLBuffer & getCLBuffer() {
+        return clBuf;
+    }
 
-	void map(uint16_t inputMin, uint16_t inputMax, uint16_t outputMin = 0, uint16_t outputMax = USHRT_MAX);
-    void toPoints(float fovH, float fovV, OpenCLBuffer & points);
-    void toPoints(float fovH, float fovV);
+protected:
+    OpenCLBuffer clBuf;
+};
+
+
+//////////////////////////////////////////////////
+// DEPTH IMAGE
+//
+// The raw image from a depth camera
+
+class ofxDepthImage {
+public:
+
+    void allocate(int width, int height);
+    bool isAllocated() const;
+    
+    int getWidth() const {
+        return width;
+    }
+    int getHeight() const {
+        return height;
+    }
+    int getNumPixels() const {
+        return width * height;
+    }
+    
+    void load(string filepath);
+
+    void write(ofShortPixels & pixels);
+    void read(ofShortPixels & pixels);
+    
+    void update();
+    void update(ofTexture & tex);
+    
+    void draw(float x, float y);
+    void draw(float x, float y, float w, float h);
+    
+    void denoise(float threshold, int neighbours = 1);
+    void denoise(float threshold, int neighbours, ofxDepthImage & outputImage);
+    void map(uint16_t inputMin, uint16_t inputMax, uint16_t outputMin = 0, uint16_t outputMax = USHRT_MAX);
+    void map(uint16_t inputMin, uint16_t inputMax, uint16_t outputMin, uint16_t outputMax, ofxDepthImage & outputImage);
+    void toPoints(float fovH, float fovV, ofxDepthPoints & points);
+    void toPoints(ofxDepthTable & depthTable, ofxDepthPoints & points);
+
+    OpenCLBuffer & getCLBuffer() {
+        return clBuf;
+    }
+
+protected:
+    int width;
+    int height;
+    ofBufferObject glBuf;
+    OpenCLBuffer clBuf;
+    ofTexture tex;
+};
+
+//////////////////////////////////////////////////
+// DEPTH DATA
+
+class ofxDepthData {
+public:
+    void allocate(int size);
+    
+    int getSize() const {
+        return data.size();
+    }
+    int getCount() const {
+        return count;
+    }
+    void setCount(int count) {
+        this->count = count;
+    }
+
+    vector<ofVec4f> & getData() {
+        return data;
+    }
+    
+    void getMinMax(ofVec4f & min, ofVec4f & max);
+
+    int countZeros();
+    int removeZeros(bool resize = true);
+    int removeZeros(bool resize, ofxDepthData & outputData);
+
+protected:
+    vector<ofVec4f> data;
+    int count;
+};
+
+
+//////////////////////////////////////////////////
+// DEPTH POINTS
+
+class ofxDepthPoints {
+public:
+
+    void allocate(int size);
+    bool isAllocated();
+    
+    int getSize() const {
+        return size;
+    }
+    int getCount() const {
+        return count;
+    }
+    void setCount(int count) {
+        this->count = count;
+    }
+    
+    void load(string filepath);
+
+    void read(ofxDepthData & data);
+    void read(vector<ofVec4f> & points);
+    void write(ofxDepthData & data);
+    void write(vector<ofVec4f> & points);
+    void write(vector<ofVec4f> & points, int count);
+    
+    void draw();
+    
+    void transform(const ofMatrix4x4 & mat);
+    void transform(const ofMatrix4x4 & mat, ofxDepthPoints & outputPoints);
+    
+    OpenCLBuffer & getCLBuffer() {
+        return clBuf;
+    }
+
+protected:
+    int size;
+    int count;
+    ofBufferObject glBuf;
+    OpenCLBuffer clBuf;
+    ofVbo vbo;
+    
+    OpenCLBufferManagedT<ofVec4f> matrix;
+};
+
+//////////////////////////////////////////////////
+// DEPTH CORE
+
+class ofxDepthCore {
+public:
+    static ofxDepthCore & get() {
+        static ofxDepthCore core;
+        return core;
+    }
+
+	void setup(int deviceNumber = -1);
+    void setup(string vendorName, string deviceName = "");
+    bool isSetup();
+    
+    OpenCL & getCL();
+    OpenCLKernelPtr getKernel(string name);
 
 private:
+    ofxDepthCore() {}
 	OpenCL opencl;
-	ofBufferObject texObj;
-    ofBufferObject pntObj;
-	OpenCLBuffer depth;
-    OpenCLBuffer points;
-	int width;
-	int height;
 };
+
+static ofxDepthCore & ofxDepth = ofxDepthCore::get();
